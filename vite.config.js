1c5e86e01932ac19d0fd2de3ed0b3cea5d7a8274
cristimanea26 <111192923+cristimanea26@users.noreply.github.com>
2022-10-09 21:06:50 +0300

    robofriends with hooks

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: 'https://github.com/cristimanea26/rf_hooks',
+  plugins: [react()]
+})
